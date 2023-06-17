e2d5aaeb183ea22b6161be3f729b6da759ba4a56
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-06-17 15:22:59 -0700

    Update trial columns

--- src/np_nwb/dynamic_routing/trials.py
+++ src/np_nwb/dynamic_routing/trials.py
@@ -310,7 +310,7 @@ class DRDataLoader:
                     logger.warning(f'Mismatch in stim block {idx = }: {len(diode_flips) = }, {len(vsyncs) = }')
                 
                     if len(diode_flips) > len(vsyncs):
-                        logger.warning('Cutting excess diode flips at number of vsync')
+                        logger.warning('Cutting excess diode flips at length of vsyncs')
                         diode_flips = diode_flips[:len(vsyncs)]
                     else:
                         raise IndexError('Fewer diode flips than vsyncs: needs investigating')
@@ -318,7 +318,7 @@ class DRDataLoader:
                 pass
                 # TODO adjust frametimes with diode data when flip is every 1 s
 
-            # intervals are in a bimodal distribution due to asymmetry of
+            # intervals have bimodal distribution due to asymmetry of
             # photodiode thresholding: adjust intervals to get a closer
             # estimate of actual transition time 
             intervals = np.diff(diode_flips)
@@ -349,21 +349,23 @@ class DRDataLoader:
             return self.frametime_blocks.main
         else:
             assert self.sam is not None
-            monitor_latency = 0.027 # s
+            monitor_latency = 0.028 # s
             # 0.020 s avg to start refreshing (top line of monitor)
-            # 0.007 s (after the top line) for center of monitor to refresh
+            # 0.008 s (after the top line) for center of monitor to refresh
             return self.sam.trialStartTimes + monitor_latency
 
-    #TODO get RF frametimes - cannot load sam's obj currently
+    @property
+    def rf_frametimes(self) -> Sequence[float]:
+        if not self.sync:
+            raise AttributeError(f'No sync file: rf times only available for ecephys experiments with sync: {self.session}')
+        
+        if not self.frametime_blocks.rf:
+            raise AttributeError(f'A block of frametimes corresponding to rf mapping was not found for this experiment: {self.session}')
+       
+        return self.frametime_blocks.rf
     
 import functools
 
-def convert_frame_times(method):
-    @functools.wraps(method)
-    def _impl(self):
-        frame_numbers = method(self)
-        return self._data.task_frametimes[frame_numbers]
-    return _impl
 
 class DRTaskTrials(PropertyDict):
     """All property getters without a leading underscore will be
@@ -382,28 +384,282 @@ class DRTaskTrials(PropertyDict):
         
     """
     
+    _sam: DynRoutData
+    _data: DRDataLoader
+    
     def __init__(self, session: str | pathlib.Path | np_session.Session, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self._data = DRDataLoader(session)
         assert self._data.sam is not None
+        self._sam = self._data.sam
+        
+    def get_frametimes(self, frame_idx: Sequence[float]):
+        return np.array(self._data.task_frametimes)[frame_idx]
+
+    # ---------------------------------------------------------------------- #
     
-    @convert_frame_times
+    @property
+    def is_response(self) -> Sequence[bool]:
+        """The subject licked one or more times during the response window."""
+        return self._sam.trialResponse
+
+    @property
+    def is_hit(self) -> Sequence[bool]:
+        """A response in a GO trial."""
+        return self.is_response & self.is_go
+    
+    @property
+    def is_false_alarm(self) -> Sequence[bool]:
+        """A response in a NOGO trial."""
+        return self.is_response & self.is_nogo
+    
+    @property
+    def is_correct_reject(self) -> Sequence[bool]:
+        """No response in a NOGO trial.
+        
+        - excludes catch trials
+        """
+        return ~self.is_response & self.is_nogo
+    
+    @property
+    def is_miss(self) -> Sequence[bool]:
+        """No response in a GO trial."""
+        return ~self.is_response & self.is_go
+    
+    
+    @property
+    def is_go(self) -> Sequence[bool]:
+        """Condition in which the subject should respond.
+    
+        - target stim presented in rewarded context block
+        """
+        return self._sam.goTrials
+    
+    @property
+    def is_nogo(self) -> Sequence[bool]:
+        """Condition in which the subject should not respond.
+        
+        - non-target stim presented in any context block
+        - target stim presented in non-rewarded context block
+        - excludes catch trials
+        """
+        return self._sam.nogoTrials
+    
+    @property
+    def is_rewarded(self) -> Sequence[bool]:
+        """The subject received a reward.
+        
+        - includes non-contingent rewards
+        """
+        return self._sam.trialRewarded
+    
+    @property
+    def is_noncontingent_reward(self) -> Sequence[bool]:
+        """The subject received a reward that did not depend on its response."""        
+        return self._sam.autoRewarded
+
+    @property
+    def is_contingent_reward(self) -> Sequence[bool]:
+        """The subject received a reward for a correct response."""        
+        return self.is_rewarded & self.is_hit
+    
+    @property
+    def is_reward_scheduled(self) -> Sequence[bool]:
+        """A non-contingent reward was scheduled to occur.
+        
+        - subject may have responded correctly and received contingent reward
+          instead
+        """
+        return self._sam.autoRewardScheduled
+    
+    @property
+    def _aud_stims(self) -> Sequence[str]:
+        return np.unique([stim for stim in self._sam.trialStim if 'sound' in stim.lower()])
+    
+    @property
+    def _vis_stims(self) -> Sequence[str]:
+        return np.unique([stim for stim in self._sam.trialStim if 'vis' in stim.lower()])
+    
+    @property
+    def _targets(self) -> Sequence[str]:
+        return np.unique([stim for stim in self._sam.blockStimRewarded])
+    
+    @property
+    def _aud_targets(self) -> Sequence[str]:
+        return np.unique([stim for stim in self._aud_stims if stim in self._targets])
+    
+    @property
+    def _vis_targets(self) -> Sequence[str]:
+        return np.unique([stim for stim in self._vis_stims if stim in self._targets])
+    
+    @property
+    def _aud_nontargets(self) -> Sequence[str]:
+        return np.unique([stim for stim in self._aud_stims if stim not in self._targets])
+    
+    @property
+    def _vis_nontargets(self) -> Sequence[str]:
+        return np.unique([stim for stim in self._vis_stims if stim not in self._targets])
+    
+    
+    
+    @property
+    def is_aud_stim(self) -> Sequence[bool]:
+        """An auditory stimulus was presented.
+        
+        - target and non-target stimuli
+        - rewarded and unrewarded contexts
+        """
+        return np.isin(self._sam.trialStim, self._aud_stims)
+    
+    @property
+    def is_vis_stim(self) -> Sequence[bool]:
+        """A visual stimulus was presented.
+        
+        - target and non-target stimuli
+        - rewarded and unrewarded contexts
+        """
+        return np.isin(self._sam.trialStim, self._aud_stims)
+    
+    @property
+    def is_catch(self) -> Sequence[bool]:
+        """No stimulus was presented."""
+        return np.isin(self._sam.trialStim, 'catch')
+    
+    @property
+    def is_aud_target(self) -> Sequence[bool]:
+        """An auditory stimulus was presented that the subject should respond to in a certain context."""
+        return np.isin(self._sam.trialStim, self._aud_targets)
+    
+    @property
+    def is_vis_target(self) -> Sequence[bool]:
+        """A visual stimulus was presented that the subject should respond to in a certain context."""
+        return np.isin(self._sam.trialStim, self._vis_targets)
+    
+    @property
+    def is_aud_nontarget(self) -> Sequence[bool]:
+        """An auditory stimulus was presented that the subject should never respond to."""
+        return np.isin(self._sam.trialStim, self._aud_nontargets)
+    
+    @property
+    def is_vis_nontarget(self) -> Sequence[bool]:
+        """A visual stimulus was presented that the subject should never respond to."""
+        return np.isin(self._sam.trialStim, self._vis_nontargets)
+    
+    @property
+    def block_idx(self) -> Sequence[int]:
+        """1-indexed block number, increments with each b."""
+        assert 0 not in self._sam.trialBlock
+        return self._sam.trialBlock
+    
+    @property
+    def idx_within_block(self) -> Sequence[int]:
+        """1-indexed trial number within a block, increments over the block.
+        
+        # TODO aborted trials not tracked
+        """
+        return 1 + np.concatenate([np.arange(count) for count in np.unique(self.block_idx, return_counts=True)[1]])
+    
+    @property
+    def scheduled_reward_idx_within_block(self) -> Sequence[int | float]:
+        """
+        # TODO check w/Sam - changed from 'noncontingent_reward_idx_within_block'
+        
+        """
+        return np.where(self.is_reward_scheduled == True, self.idx_within_block, np.nan * np.ones_like(self.idx_within_block))
+    
+    @property
+    def idx_with_stim(self) -> Sequence[int]:
+        """1-indexed trial number for trials with stimuli, increments over
+        time.
+        
+        - excludes catch trials
+        - excludes aborted trials
+        """
+        return 1 + np.concatenate([np.arange(count) for count in np.unique(self.block_idx, return_counts=True)[1]])
+    
+    
+    @property
+    def _rewarded_stim(self) -> Sequence[str]:
+        return self._sam.blockStimRewarded[self.block_idx - 1]
+    
+    @property
+    def is_vis_context(self) -> Sequence[bool]:
+        """Trial occurs within a block in which the subject should respond to
+        a visual target."""
+        return np.isin(self._rewarded_stim, self._vis_stims)
+    
+    @property
+    def is_aud_context(self) -> Sequence[bool]:
+        """Trial occurs within a block in which the subject should respond to
+        a auditory target."""
+        return np.isin(self._rewarded_stim, self._aud_stims)
+    
+    @property
+    def is_repeat(self) -> Sequence[bool]:
+        """The trial is a repetition of the previous trial, which resulted in a
+        miss.
+        
+        - always False on first trial after context block switch
+        """
+        return self._sam.trialRepeat
+    
+    @property
+    def is_aborted(self) -> Sequence[bool]:
+        """The subject licked one or more times during the quiescent period.
+        
+        - trial ends at quiescent period stop time
+        # TODO
+        """
+        return np.zeros_like(self._sam.trialStartTimes, dtype=bool)
+    
+    @property
+    def is_opto(self) -> Sequence[bool]:
+        """Optogenetic inactivation was applied during the trial."""
+        return np.isnan(self.opto_start_time)
+    
+    @property
+    def is_context_switch(self) -> Sequence[bool]:
+        """The first trial with a stimulus after a change in context.
+        
+        - excludes aborted trials
+        """
+        return np.isin(self.idx_within_block, 1)
+    
+    """
+    @property
+    def is_(self) -> Sequence[bool]:
+        """"""
+        return 
+    """
+    
+    
     @property
     def start_time(self) -> Sequence[float]:
-        "Earliest time in trial, before any events occur."
-        return self._data.sam.trialStartFrame
+        "Earliest time in each trial, before any events occur."
+        return self.get_frametimes(self._sam.trialStartFrame)
+    
+    @property
+    def stim_start_time(self) -> Sequence[float]:
+        "Onset of visual or auditory stimulus."
+        # TODO get corrected aud onset: switch on trial type
+        return self.get_frametimes(self._sam.stimStartFrame)
+    
+    @property
+    def opto_start_time(self) -> Sequence[float]:
+        "Onset of optogenetic inactivation."
+        # TODO
+        return np.nan * np.ones_like(self._sam.stimStartFrame)
 
     @property
     def stop_time(self) -> Sequence[float]:
-        return self._data.sam.trialEndFrame
+        "Latest time each trial, after all events have occurred."
+        return self.get_frametimes(self._sam.trialEndFrame)
 
 
 if __name__ == "__main__":
     doctest.testmod()
     
-    # x = DRTaskTrials(
-    #     "DRpilot_626791_20220817"
-    #     )
-    # x._data.frametime_blocks
-    # x.to_add_trial_columns()
+    x = DRTaskTrials("DRpilot_626791_20220817")
+    x.start_time
+    x.to_add_trial_columns()
     # x.to_add_trials()
