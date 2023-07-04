0608bc707ba72aba57b7d492d7f7815a3cc8f955
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-07-04 15:09:45 -0700

    Fix and cache properties

--- src/np_nwb/trials/DRtask.py
+++ src/np_nwb/trials/DRtask.py
@@ -76,11 +76,11 @@ class DRTaskTrials(PropertyDict):
     # ---------------------------------------------------------------------- #
     # helper-properties that won't become columns:
     
-    @property
+    @functools.cached_property
     def _has_opto(self) -> bool:
         return hasattr(self._sam, 'optoVoltage') and any(self._sam.optoVoltage)
 
-    @property
+    @functools.cached_property
     def _first_vsync_time(self) -> float:
         return self._data.vsync_time_blocks.task[0]
     
@@ -93,7 +93,7 @@ class DRTaskTrials(PropertyDict):
     def _h5(self) -> h5py.File:
         return self._data.task
     
-    @property
+    @functools.cached_property
     def _len(self) -> int:
         """Number of trials, cached"""
         with contextlib.suppress(AttributeError):
@@ -101,42 +101,42 @@ class DRTaskTrials(PropertyDict):
         self._length = len(self.start_time)
         return self._len
     
-    @property
+    @functools.cached_property
     def _aud_stims(self) -> Sequence[str]:
         return np.unique([stim for stim in self.stim_name if 'sound' in stim.lower()])
     
-    @property
+    @functools.cached_property
     def _vis_stims(self) -> Sequence[str]:
         return np.unique([stim for stim in self._sam.trialStim if 'vis' in stim.lower()])
     
-    @property
+    @functools.cached_property
     def _targets(self) -> Sequence[str]:
         return np.unique([stim for stim in self._sam.blockStimRewarded])
     
-    @property
+    @functools.cached_property
     def _aud_targets(self) -> Sequence[str]:
         return np.unique([stim for stim in self._aud_stims if stim in self._targets])
     
-    @property
+    @functools.cached_property
     def _vis_targets(self) -> Sequence[str]:
         return np.unique([stim for stim in self._vis_stims if stim in self._targets])
     
-    @property
+    @functools.cached_property
     def _aud_nontargets(self) -> Sequence[str]:
         return np.unique([stim for stim in self._aud_stims if stim not in self._targets])
     
-    @property
+    @functools.cached_property
     def _vis_nontargets(self) -> Sequence[str]:
         return np.unique([stim for stim in self._vis_stims if stim not in self._targets])
     
-    @property
+    @functools.cached_property
     def _trial_rewarded_stim_name(self) -> Sequence[str]:
         return self._sam.blockStimRewarded[self.block_index]
     
     # ---------------------------------------------------------------------- #
     # times:
         
-    @property
+    @functools.cached_property
     def start_time(self) -> Sequence[float]:
         """Earliest time in each trial, before any events occur.
         
@@ -148,7 +148,7 @@ class DRTaskTrials(PropertyDict):
             self._sam.stimStartFrame - self._h5['preStimFramesFixed'][()]
         )
 
-    @property
+    @functools.cached_property
     def quiescent_start_time(self) -> Sequence[float]:
         """Start of period in which the subject should not lick, otherwise the
         trial will be aborted and start over.
@@ -160,30 +160,7 @@ class DRTaskTrials(PropertyDict):
             self._sam.stimStartFrame - self._h5['quiescentFrames'][()]
         )
     
-    @property
-    def quiescent_stop_time(self) -> Sequence[float]:
-        """End of period in which the subject should not lick, otherwise the
-        trial will be aborted and start over."""
-        return self.get_display_times(
-            self._sam.stimStartFrame
-        )
-        
-    @property
-    def response_window_start_time(self) -> Sequence[float]:
-        """"""
-        return self.get_display_times(
-            self._sam.stimStartFrame + self._h5['responseWindow'][()][0]
-        )
-        
-    @property
-    def response_time(self) -> Sequence[float]:
-        """Time of first lick in the response window, or NaN if no lick occurred."""
-        return self.get_display_times(
-            self._h5['trialResponseFrame'][()]
-        )
-        
-    
-    @property
+    @functools.cached_property
     def opto_start_time(self) -> Sequence[float]:
         """Onset of optogenetic inactivation."""
         if not self._has_opto:
@@ -195,15 +172,16 @@ class DRTaskTrials(PropertyDict):
             ),
             np.nan * np.ones(self._len),
         )
-
-    @property
-    def opto_stop_time(self) -> Sequence[float]:
-        """Offset of optogenetic inactivation."""
-        if not self._has_opto:
-            return np.nan * np.ones(self._len)
-        return self.opto_start_time + self._sam.trialOptoDur
     
-    @property
+    @functools.cached_property
+    def quiescent_stop_time(self) -> Sequence[float]:
+        """End of period in which the subject should not lick, otherwise the
+        trial will be aborted and start over."""
+        return self.get_display_times(
+            self._sam.stimStartFrame
+        )
+        
+    @functools.cached_property
     def stim_start_time(self) -> Sequence[float]:
         """Onset of visual or auditory stimulus."""
         starts = np.nan * np.ones(self._len)
@@ -215,10 +193,18 @@ class DRTaskTrials(PropertyDict):
             if self.is_aud_stim[idx]:
                 starts[idx] = self._data.task_sound_on_off[idx][0]
         return starts
-
-    @property
+        
+    @functools.cached_property
+    def response_window_start_time(self) -> Sequence[float]:
+        """Start of window in which the subject should lick if a GO trial,
+        otherwise should not lick."""
+        return self.get_display_times(
+            self._sam.stimStartFrame + self._h5['responseWindow'][()][0]
+        )
+    
+    @functools.cached_property
     def stim_stop_time(self) -> Sequence[float]:
-        """TODO"""
+        """Offset of visual or auditory stimulus."""
         ends = np.nan * np.ones(self._len)
         for idx in range(self._len):
             if self.is_vis_stim[idx]:
@@ -228,29 +214,45 @@ class DRTaskTrials(PropertyDict):
             if self.is_aud_stim[idx]:
                 ends[idx] = self._data.task_sound_on_off[idx][1]
         return ends
-        
-    @property
+    
+    @functools.cached_property
+    def response_time(self) -> Sequence[float]:
+        """Time of first lick in the response window, or NaN if no lick occurred."""
+        return self.get_display_times(
+            self._sam.trialResponseFrame
+        )
+
+    @functools.cached_property
     def response_window_stop_time(self) -> Sequence[float]:
-        """"""
+        """End of window in which the subject should lick if a GO trial,
+        otherwise should not lick."""
         return self.get_display_times(
             self._sam.stimStartFrame + self._h5['responseWindow'][()][1]
         )
         
-    @property
+    @functools.cached_property
+    def opto_stop_time(self) -> Sequence[float]:
+        """Offset of optogenetic inactivation."""
+        if not self._has_opto:
+            return np.nan * np.ones(self._len)
+        return self.opto_start_time + self._sam.trialOptoDur
+        
+    @functools.cached_property
     def post_response_window_start_time(self) -> Sequence[float]:
-        """"""
+        """Start of null period in which the subject awaits a new trial or a
+        timeout period; may receive a noncontingent reward if scheduled."""
         return self.response_window_stop_time
     
-    @property
+    @functools.cached_property
     def post_response_window_stop_time(self) -> Sequence[float]:
         """"""
         return self.get_display_times(
             self._sam.stimStartFrame + self._h5['postResponseWindowFrames'][()]
         )
     
-    @property
+    @functools.cached_property
     def timeout_start_time(self) -> Sequence[float]:
-        """"""
+        """Start of extended inter-trial period added due to a false alarm."""
         starts = np.nan * np.ones_like((self.start_time))
         for idx in range(0, self._len - 1):
             if self.is_repeat[idx + 1]:
@@ -260,7 +262,7 @@ class DRTaskTrials(PropertyDict):
                 )
         return starts
     
-    @property
+    @functools.cached_property
     def timeout_stop_time(self) -> Sequence[float]:
         """"""
         ends = np.nan * np.ones_like((self.start_time))
@@ -272,13 +274,13 @@ class DRTaskTrials(PropertyDict):
         return ends
 
     '''
-    @property
+    @functools.cached_property
     def _time(self) -> Sequence[float]:
         """TODO"""
         return np.nan * np.zeros(self._len)
     '''
 
-    @property
+    @functools.cached_property
     def stop_time(self) -> Sequence[float]:
         """Latest time in each trial, after all events have occurred."""
         return self.get_display_times(self._sam.trialEndFrame)
@@ -287,12 +289,12 @@ class DRTaskTrials(PropertyDict):
     # ---------------------------------------------------------------------- #
     # parameters:
         
-    @property
+    @functools.cached_property
     def stim_name(self) -> Sequence[str]:
         """TODO"""
         return self._sam.trialStim
     
-    @property
+    @functools.cached_property
     def stim_index(self) -> Sequence[float]:
         """0-indexed stim number, randomized over trials.
         
@@ -303,18 +305,18 @@ class DRTaskTrials(PropertyDict):
         return np.unique(self.stim_name).searchsorted(self.stim_name)
     
     
-    @property
+    @functools.cached_property
     def block_index(self) -> Sequence[int]:
         """0-indexed block number, increments with each block."""
         assert min(self._sam.trialBlock) == 1
         return self._sam.trialBlock - 1
     
-    @property
+    @functools.cached_property
     def context_name(self) -> Sequence[str]:
         """TODO"""
         return np.array([name[:-1] for name in self._trial_rewarded_stim_name])
     
-    @property
+    @functools.cached_property
     def index(self) -> Sequence[int]:
         """0-indexed trial number for regular trials (with stimuli), increments over
         time.
@@ -329,12 +331,12 @@ class DRTaskTrials(PropertyDict):
                 counter += 1
         return regular_trial_index
     
-    @property
+    @functools.cached_property
     def index_within_block(self) -> Sequence[int]:
         """0-indexed trial number within a block, increments over the block."""
         return np.concatenate([np.arange(count) for count in np.unique(self.block_index, return_counts=True)[1]])
     
-    @property
+    @functools.cached_property
     def scheduled_reward_index_within_block(self) -> Sequence[float]:
         """
         # TODO check w/Sam
@@ -343,13 +345,13 @@ class DRTaskTrials(PropertyDict):
         """
         return np.where(self.is_reward_scheduled == True, self.index_within_block, np.nan * np.ones(self._len))
 
-    @property
+    @functools.cached_property
     def TODO_opto_area_name(self) -> Sequence[str]:
         """Target location for optogenetic inactivation during the trial."""
         # TODO
         return np.full(self._len, np.nan)
     
-    @property
+    @functools.cached_property
     def TODO_opto_area_index(self) -> Sequence[int | float]:
         """Target location for optogenetic inactivation during the trial.
         
@@ -358,25 +360,25 @@ class DRTaskTrials(PropertyDict):
         # TODO
         return np.full(self._len, np.nan)
     
-    @property
+    @functools.cached_property
     def opto_voltage(self):
         if not any(self.is_opto):
             return np.full(self._len, np.nan)
         return self._sam.trialOptoVoltage
     
-    @property
+    @functools.cached_property
     def galvo_voltage_x(self):
         if not any(self.is_opto):
             return np.full(self._len, np.nan)
         return np.array([voltage[0] for voltage in self._sam.trialGalvoVoltage])
     
-    @property
+    @functools.cached_property
     def galvo_voltage_y(self):
         if not any(self.is_opto):
             return np.full(self._len, np.nan)
         return np.array([voltage[1] for voltage in self._sam.trialGalvoVoltage])
     
-    @property
+    @functools.cached_property
     def repeat_number(self) -> Sequence[float]:
         """Number of times the trial has already been presented in immediately
         preceding trials (repeated due to false alarm).
@@ -396,12 +398,12 @@ class DRTaskTrials(PropertyDict):
     # ---------------------------------------------------------------------- #
     # bools:
     
-    @property
+    @functools.cached_property
     def is_response(self) -> Sequence[bool]:
         """The subject licked one or more times during the response window."""
         return self._sam.trialResponse
 
-    @property
+    @functools.cached_property
     def is_correct(self) -> Sequence[bool]:
         """The subject acted correctly in the response window, according to its
         training.
@@ -410,7 +412,7 @@ class DRTaskTrials(PropertyDict):
         """
         return self.is_hit | self.is_correct_reject | (self.is_catch & ~self.is_response)
     
-    @property
+    @functools.cached_property
     def is_incorrect(self) -> Sequence[bool]:
         """The subject acted incorrectly in the response window, according to its
         training.
@@ -419,17 +421,17 @@ class DRTaskTrials(PropertyDict):
         """
         return self.is_miss | self.is_false_alarm | (self.is_catch & self.is_response)
     
-    @property
+    @functools.cached_property
     def is_hit(self) -> Sequence[bool]:
         """The subject responded in a GO trial."""
         return self.is_response & self.is_go
     
-    @property
+    @functools.cached_property
     def is_false_alarm(self) -> Sequence[bool]:
         """The subject responded in a NOGO trial."""
         return self.is_response & self.is_nogo
     
-    @property
+    @functools.cached_property
     def is_correct_reject(self) -> Sequence[bool]:
         """The subject did not respond in a NOGO trial.
         
@@ -437,12 +439,12 @@ class DRTaskTrials(PropertyDict):
         """
         return ~self.is_response & self.is_nogo
     
-    @property
+    @functools.cached_property
     def is_miss(self) -> Sequence[bool]:
         """The subject did not respond in a GO trial."""
         return ~self.is_response & self.is_go
     
-    @property
+    @functools.cached_property
     def is_go(self) -> Sequence[bool]:
         """Condition in which the subject should respond.
     
@@ -450,7 +452,7 @@ class DRTaskTrials(PropertyDict):
         """
         return self._sam.goTrials
     
-    @property
+    @functools.cached_property
     def is_nogo(self) -> Sequence[bool]:
         """Condition in which the subject should not respond.
         
@@ -460,7 +462,7 @@ class DRTaskTrials(PropertyDict):
         """
         return self._sam.nogoTrials
     
-    @property
+    @functools.cached_property
     def is_rewarded(self) -> Sequence[bool]:
         """The subject received a reward.
         
@@ -468,17 +470,17 @@ class DRTaskTrials(PropertyDict):
         """
         return self._sam.trialRewarded
     
-    @property
+    @functools.cached_property
     def is_noncontingent_reward(self) -> Sequence[bool]:
         """The subject received a reward that did not depend on its response."""        
         return self._sam.autoRewarded
 
-    @property
+    @functools.cached_property
     def is_contingent_reward(self) -> Sequence[bool]:
         """The subject received a reward for a correct response in a GO trial."""     
         return self.is_rewarded & self.is_hit
     
-    @property
+    @functools.cached_property
     def is_reward_scheduled(self) -> Sequence[bool]:
         """A non-contingent reward was scheduled to occur.
         
@@ -487,7 +489,7 @@ class DRTaskTrials(PropertyDict):
         """
         return self._sam.autoRewardScheduled
     
-    @property
+    @functools.cached_property
     def is_aud_stim(self) -> Sequence[bool]:
         """An auditory stimulus was presented.
         
@@ -496,7 +498,7 @@ class DRTaskTrials(PropertyDict):
         """
         return np.isin(self._sam.trialStim, self._aud_stims)
     
-    @property
+    @functools.cached_property
     def is_vis_stim(self) -> Sequence[bool]:
         """A visual stimulus was presented.
         
@@ -505,44 +507,44 @@ class DRTaskTrials(PropertyDict):
         """
         return np.isin(self._sam.trialStim, self._vis_stims)
     
-    @property
+    @functools.cached_property
     def is_catch(self) -> Sequence[bool]:
         """No stimulus was presented."""
         return np.isin(self._sam.trialStim, 'catch')
     
-    @property
+    @functools.cached_property
     def is_aud_target(self) -> Sequence[bool]:
         """An auditory stimulus was presented that the subject should respond to in a certain context."""
         return np.isin(self._sam.trialStim, self._aud_targets)
     
-    @property
+    @functools.cached_property
     def is_vis_target(self) -> Sequence[bool]:
         """A visual stimulus was presented that the subject should respond to in a certain context."""
         return np.isin(self._sam.trialStim, self._vis_targets)
     
-    @property
+    @functools.cached_property
     def is_aud_nontarget(self) -> Sequence[bool]:
         """An auditory stimulus was presented that the subject should never respond to."""
         return np.isin(self._sam.trialStim, self._aud_nontargets)
     
-    @property
+    @functools.cached_property
     def is_vis_nontarget(self) -> Sequence[bool]:
         """A visual stimulus was presented that the subject should never respond to."""
         return np.isin(self._sam.trialStim, self._vis_nontargets)
     
-    @property
+    @functools.cached_property
     def is_vis_context(self) -> Sequence[bool]:
         """Trial occurs within a block in which the subject should respond to
         a visual target."""
         return np.isin(self._trial_rewarded_stim_name, self._vis_stims)
     
-    @property
+    @functools.cached_property
     def is_aud_context(self) -> Sequence[bool]:
         """Trial occurs within a block in which the subject should respond to
         a auditory target."""
         return np.isin(self._trial_rewarded_stim_name, self._aud_stims)
     
-    @property
+    @functools.cached_property
     def is_repeat(self) -> Sequence[bool]:
         """The trial is a repetition of the previous trial, which resulted in a
         miss.
@@ -552,18 +554,18 @@ class DRTaskTrials(PropertyDict):
         """
         return self._sam.trialRepeat
     
-    @property
+    @functools.cached_property
     def is_opto(self) -> Sequence[bool]:
         """Optogenetic inactivation was applied during the trial."""
         return ~np.isnan(self.opto_start_time)
     
-    @property
+    @functools.cached_property
     def is_context_switch(self) -> Sequence[bool]:
         """The first trial with a stimulus after a change in context."""
         return np.isin(self.index_within_block, 1)
     
     """
-    @property
+    @functools.cached_property
     def is_(self) -> Sequence[bool]:
         """"""
         return 
